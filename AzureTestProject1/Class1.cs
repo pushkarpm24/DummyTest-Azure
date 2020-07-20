52d48571d29eb19958d122d6383c207bb3cd1d23
pushkarpm24 <pushkarmorey555@gmail.com>
2020-07-20 08:59:19 +0530

    ADDED:Test Case To Login Facebook

---
+++ AzureTestProject1/Class1.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureTestProject1
+{
+    public class Class1
+    {
+        IWebDriver driver;
+        public Class1(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.Id, Using = "email")]
+        public IWebElement EmailTextbox;
+        [FindsBy(How = How.Id, Using = "pass")]
+        public IWebElement PassTextbox;
+        [FindsBy(How = How.XPath, Using = "//*[@id='u_0_b']")]
+        public IWebElement LoginButton;
+
+        public void GoToHomePage()
+        {
+            EmailTextbox.SendKeys("pushkarmorey555@gmail.com");
+            PassTextbox.SendKeys("pushkarpm2430");
+            LoginButton.Click();
+        }
+    }
+}
---
+++ AzureTestProject1/TestClass.cs
@@ -0,0 +1,54 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AzureTestProject1
+{
+    [TestFixture]
+    public class TestClass
+    {
+        public IWebDriver driver;
+
+        [OneTimeSetUp]
+        public void Initilize()
+        {
+
+            //using chrome options to disable unwanted notifications
+            ChromeOptions opt = new ChromeOptions();
+            opt.AddArguments("--disable-notifications");
+
+            //Launch the chrome browser
+            driver = new ChromeDriver(opt);
+
+            //Using implicitly wait 
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+
+            //Maximizing the window
+            driver.Manage().Window.Maximize();
+
+            //Enter the url            
+            driver.Url = "https://www.facebook.com";
+        }
+
+        [Test]
+        public void Login()
+        {
+            Class1 page = new Class1(driver);
+            page.GoToHomePage();
+        }
+
+
+        [OneTimeTearDown]
+        public void Close()
+        {
+            Thread.Sleep(5000);
+            driver.Quit();
+        }
+    }
+}
